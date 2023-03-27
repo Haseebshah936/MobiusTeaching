aea2bf3a0f47d1113d0251128a4f86afa77edfae
Haseeb shah <haseebshah936@gmail.com>
2023-03-27 16:39:26 +0500

    minor fixes

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { Auth, onAuthStateChanged } from "firebase/auth";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
+import { onAuthStateChanged } from "firebase/auth";
+import { NavigationContainer } from "@react-navigation/native";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import * as SecureStore from "expo-secure-store";
+
 import { auth, db } from "./src/config/firebase";
 import AuthNavigator from "./src/navigation/AuthNavigator";
 import AppNavigator from "./src/navigation/AppNavigator";
-import { NavigationContainer } from "@react-navigation/native";
 import { CustomContextProvider } from "./src/hooks/useCustomContext";
-import { doc, getDoc, setDoc } from "firebase/firestore";
 import CreateProfileStack from "./src/navigation/CreateProfileStack";
 import { Loading } from "./src/screens/Auth";
-import * as SecureStore from "expo-secure-store";
 import { signIn } from "./src/config/firebase/functions";
-import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
 const App = () => {
   const [user, setUser] = useState(null);
